7b155bcd682d882ece97a6873bd3a62fffb3b0cc
Nity6000 <31748962+Nity6000@users.noreply.github.com>
2017-12-30 20:56:13 -0500

    Adding all the files.
    Wow my source is 20,000 files.

---
+++ Assets/ChangeScene.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+using System.Collections;
+
+public class ChangeScene : MonoBehaviour {
+
+    private void Update()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void SceneChange(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void CloseGame()
+    {
+        Application.Quit();
+    }
+}
